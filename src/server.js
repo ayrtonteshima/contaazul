29b839818f5f0ad19cc45fa5f8d174fabd4b6dd5
ayrtonteshima <ayrtonteshima@gmail.com>
2017-10-01 14:23:00 -0300

    Adicionando body-parser ao projeto

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const routes = require('./routes');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static('./dist'));
 app.use(express.static('./static'));
