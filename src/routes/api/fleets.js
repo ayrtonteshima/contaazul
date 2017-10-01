d56a4aac3555671ff413a69f6a11520315e280fc
ayrtonteshima <ayrtonteshima@gmail.com>
2017-10-01 13:25:42 -0300

    Mudando nome dos arquivos - adicionando sufixo

--- src/routes/api/fleets.js
+++ src/routes/api/fleets.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { handleGetFleets } = require('./../../app/handlers/fleets');
+const { handleGetFleets } = require('./../../app/handlers/fleetsHandler');
 
 router.get('/api/v1/fleets', handleGetFleets);
 
