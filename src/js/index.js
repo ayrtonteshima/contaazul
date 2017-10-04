07e458926a72002bf7300b69c1636e9acbdc707e
ayrtonteshima <ayrtonteshima@gmail.com>
2017-10-03 23:20:09 -0300

    Refatorando pra usar compose nos renders de html

--- src/js/index.js
+++ src/js/index.js
@@ -4,23 +4,27 @@ import {
   templateItem,
   templatePagination,
 } from './template';
+import { compose } from './helpers';
 
-const renderItems = ({ data }) => {
+const renderItems = (state) => {
+  const { data } = state;
   const html = data.map(templateItem).join('');
   document.querySelector('.contaazul__table__body').innerHTML = html;
+
+  return state;
 };
 
-const renderPagination = ({ totalPages, currentPage }) => {
+const renderPagination = (state) => {
+  const { totalPages, currentPage } = state;
   const html = totalPages > 1 ?
     templatePagination(totalPages, currentPage) :
     '';
   document.querySelector('.contaazul__pagination').innerHTML = html;
+
+  return state;
 };
 
-const renderGetAllFleets = (state) => {
-  renderItems(state);
-  renderPagination(state);
-};
+const renderGetAllFleets = compose(renderPagination, renderItems);
 
 actions.getAllFleets()
   .then(renderGetAllFleets);
