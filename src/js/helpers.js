bdc9c2424b191a377fb49d2f17757109fad9ac2e
ayrtonteshima <ayrtonteshima@gmail.com>
2017-10-04 07:37:38 -0300

    Criando helper de delegação de eventos

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,1 +1,24 @@
 export const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
+
+export const delegate = (eventName, className, cb) => {
+  const checkTarget = target =>
+    target.classList && target.classList.contains(className);
+
+  document.addEventListener(eventName, (event) => {
+    let { target } = event;
+    let search = true;
+
+    if (!checkTarget(target)) {
+      while (target.className && search) {
+        target = target.parentNode;
+        if (checkTarget(target)) {
+          search = false;
+        }
+      }
+    }
+
+    if (checkTarget(target)) {
+      cb.call(target, event);
+    }
+  });
+};
