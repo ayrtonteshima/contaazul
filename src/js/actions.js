640c47d408d96d3cca12630430152989ff34cd44
ayrtonteshima <ayrtonteshima@gmail.com>
2017-10-05 00:03:20 -0300

    Refatorando actions

--- src/js/actions.js
+++ src/js/actions.js
@@ -6,49 +6,48 @@ let state;
 
 const url = `${URL_BASE_API}/fleets`;
 
+const updateState = ({ type, payload }) => {
+  state = reducers(state, { type, payload });
+  return state;
+};
+
+const dispatch = ({ type, payload }) => (
+  new Promise((resolve) => {
+    resolve(updateState({ type, payload }));
+  })
+);
+
 const getAllFleets = () => (
   new Promise((resolve) => {
-    const resolveAllFleets = (result) => {
-      state = reducers(undefined, {
+    const resolveAllFleets = result => (
+      resolve(updateState({
         type: 'GET_ALL',
         payload: result.data,
-      });
-      resolve(state);
-    };
+      }))
+    );
 
     return axios.get(url)
       .then(resolveAllFleets);
   })
 );
 
-const addItemToBeDeleted = (id) => {
-  state = reducers(state, {
-    type: 'ADD_LIST_DELETE_ITEM',
-    payload: {
-      id,
-    },
-  });
-  return Promise.resolve(state);
-};
-
 const deleteItens = () => {
   const { itensWillBeDeleted } = state;
   const mapPromises = itemId => axios.delete(`${url}/${itemId}`);
   const promiseList = Promise.all(itensWillBeDeleted.map(mapPromises));
 
-  return new Promise((resolve) => {
-    promiseList.then(() =>
-      reducers(state, {
+  const deleteFn = (resolve) => {
+    promiseList.then(() => (
+      resolve(updateState({
         type: 'DELETE_ITEM',
         payload: {
           itensIds: itensWillBeDeleted,
         },
       }))
-      .then((result) => {
-        state = result;
-        resolve(state);
-      });
-  });
+    ));
+  };
+
+  return new Promise(deleteFn);
 };
 
 const saveItem = item => (
@@ -58,37 +57,33 @@ const saveItem = item => (
     });
 
     axios.post(url, item)
-      .then(() => {
-        state = reducers(state, {
+      .then(() => (
+        resolve(updateState({
           type: 'CREATE_ITEM',
           payload: { item: itemWithId },
-        });
-        return state;
-      })
-      .then((result) => {
-        state = result;
-        resolve(state);
-      });
+        }))
+      ));
+  })
+);
+
+const addItemToBeDeleted = id => (
+  dispatch({
+    type: 'ADD_LIST_DELETE_ITEM',
+    payload: { id },
   })
 );
 
 const filter = term => (
-  new Promise((resolve) => {
-    state = reducers(state, {
-      type: 'FILTER',
-      payload: { term },
-    });
-    resolve(state);
+  dispatch({
+    type: 'FILTER',
+    payload: { term },
   })
 );
 
 const setPaginationPage = page => (
-  new Promise((resolve) => {
-    state = reducers(state, {
-      type: 'SET_PAGE',
-      payload: { page },
-    });
-    resolve(state);
+  dispatch({
+    type: 'SET_PAGE',
+    payload: { page },
   })
 );
 
