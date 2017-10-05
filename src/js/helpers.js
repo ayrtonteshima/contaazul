526004ee6b6ef7f89bb5a442ad4016fcb667c870
ayrtonteshima <ayrtonteshima@gmail.com>
2017-10-04 23:22:01 -0300

    Fix delegate

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,14 +2,14 @@ export const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args
 
 export const delegate = (eventName, className, cb) => {
   const checkTarget = target =>
-    target.classList && target.classList.contains(className);
+    target && target.classList && target.classList.contains(className);
 
   document.addEventListener(eventName, (event) => {
     let { target } = event;
     let search = true;
 
     if (!checkTarget(target)) {
-      while (target.className && search) {
+      while (target && search) {
         target = target.parentNode;
         if (checkTarget(target)) {
           search = false;
